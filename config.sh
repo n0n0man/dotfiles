5adc9cbd39c05a0e72da145ba14d7cfb15fabcd9
n0n0man <nschwarz@student.42.fr>
2017-12-14 16:42:36 +0100

    thisisacommit

--- config.sh
+++ config.sh
@@ -1,3 +1,8 @@
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -rf hybrid_material.vim ~/.vim/colors
 cp -rf .zshrc ~/
 cp -rf .vimrc ~/
-cp -rf nt9.zsh-theme $ZSH_CUSTOM/themes/
+cp -rf nt9.zsh-theme $ZSH_CUSTOM/
+open Solarized\ Dark.itermcolors
