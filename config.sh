2152adc84521f958aca815c52a6eba99b6f1e7ed
n0n0man <nschwarz@student.42.fr>
2018-03-15 10:58:18 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
